027883d4f197da19116c3e34930c007c9eab650f
ldraney <draneylucas@gmail.com>
2023-12-27 11:15:26 -0700

    app.py

---
+++ linear-plot-app/app.py
@@ -0,0 +1,18 @@
+# linear-plot-app/app.py
+
+import dash
+from dash import html
+import callbacks  # This will import your callback functions
+import layouts  # This will import your app layout
+
+app = dash.Dash(__name__)
+
+app.layout = layouts.layout  # Set the layout of the app
+
+# Include the server variable for deployment purposes
+# Please see https://dash.plotly.com/deployment
+server = app.server
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
